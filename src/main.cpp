54b52a14130d737a9bfdd923567a0cea1df07525
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-11 17:49:59 +0500

    Add bound 'Point' class

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,17 @@ namespace py = pybind11;
 #define MODULE_NAME _voronoi
 #define C_STR_HELPER(a) #a
 #define C_STR(a) C_STR_HELPER(a)
+#define POINT_NAME "Point"
+
+using coordinate_t = int;
 
 PYBIND11_MODULE(MODULE_NAME, m) {
   m.doc() = R"pbdoc(Python binding of pyvoronoi library.)pbdoc";
+
+  py::class_<Point>(m, POINT_NAME)
+      .def(py::init<coordinate_t, coordinate_t>(), py::arg("x"), py::arg("y"))
+      .def_readonly("x", &Point::X)
+      .def_readonly("y", &Point::Y);
 
 #ifdef VERSION_INFO
   m.attr("__version__") = VERSION_INFO;
