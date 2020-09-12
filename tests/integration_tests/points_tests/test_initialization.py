8f5df0d1a84c8b3dcb14b0ca28305cee62f005de
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-12 07:51:41 +0500

    Complete strategies

--- tests/integration_tests/points_tests/test_initialization.py
+++ tests/integration_tests/points_tests/test_initialization.py
@@ -6,7 +6,7 @@ from tests.utils import (BoundPoint,
 from . import strategies
 
 
-@given(strategies.integers_32, strategies.integers_32)
+@given(strategies.coordinates, strategies.coordinates)
 def test_basic(x: int, y: int) -> None:
     bound, ported = BoundPoint(x, y), PortedPoint(x, y)
 
