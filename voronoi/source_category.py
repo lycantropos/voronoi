0a9208ca4810d99f466439f915e9d6a3e851de94
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-15 21:58:48 +0500

    Complete ported 'SourceCategory' enumeration

--- voronoi/source_category.py
+++ voronoi/source_category.py
@@ -5,10 +5,10 @@ from enum import (IntEnum,
 @unique
 class SourceCategory(IntEnum):
     # point subtypes
-    SINGLE_POINT = 0x0,
-    SEGMENT_START_POINT = 0x1,
-    SEGMENT_END_POINT = 0x2,
+    SINGLE_POINT = 0x0
+    SEGMENT_START_POINT = 0x1
+    SEGMENT_END_POINT = 0x2
 
     # segment subtypes
-    INITIAL_SEGMENT = 0x8,
-    REVERSE_SEGMENT = 0x9,
+    INITIAL_SEGMENT = 0x8
+    REVERSE_SEGMENT = 0x9
