5e12e9141a6da32d7a9dc057c682afe638ab6811
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-15 18:43:45 +0500

    Add test for bound & ported 'to_orientation' functions

--- tests/integration_tests/utils_tests/strategies.py
+++ tests/integration_tests/utils_tests/strategies.py
@@ -2,8 +2,11 @@ from hypothesis import strategies
 
 from tests.strategies import (integers_32,
                               sizes)
+from tests.utils import to_bound_with_ported_points_pair
 
 coordinates = integers_32
 sizes = sizes
 floats = strategies.floats(allow_nan=False,
                            allow_infinity=False)
+points_pairs = strategies.builds(to_bound_with_ported_points_pair,
+                                 coordinates, coordinates)
---
+++ tests/integration_tests/utils_tests/test_to_orientation.py
@@ -0,0 +1,20 @@
+from _voronoi import to_orientation as bound
+from hypothesis import given
+
+from tests.utils import BoundPortedPointsPair
+from voronoi.utils import to_orientation as ported
+from . import strategies
+
+
+@given(strategies.points_pairs, strategies.points_pairs,
+       strategies.points_pairs)
+def test_basic(vertices: BoundPortedPointsPair,
+               first_ray_points: BoundPortedPointsPair,
+               second_ray_points: BoundPortedPointsPair) -> None:
+    bound_vertex, ported_vertex = vertices
+    bound_first_ray_point, ported_first_ray_point = first_ray_points
+    bound_second_ray_point, ported_second_ray_point = second_ray_points
+
+    assert (bound(bound_vertex, bound_first_ray_point, bound_second_ray_point)
+            == ported(ported_vertex, ported_first_ray_point,
+                      ported_second_ray_point))
