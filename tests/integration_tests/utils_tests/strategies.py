8a521d9bb67d1afacc46324c80dd01dfb4e41d32
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-16 11:45:14 +0500

    Complete strategies

--- tests/integration_tests/utils_tests/strategies.py
+++ tests/integration_tests/utils_tests/strategies.py
@@ -1,12 +1,12 @@
 from hypothesis import strategies
 
-from tests.strategies import (integers_32,
+from tests.strategies import (doubles,
+                              integers_32,
                               sizes)
 from tests.utils import to_bound_with_ported_points_pair
 
 coordinates = integers_32
 sizes = sizes
-floats = strategies.floats(allow_nan=False,
-                           allow_infinity=False)
+floats = doubles
 points_pairs = strategies.builds(to_bound_with_ported_points_pair,
                                  coordinates, coordinates)
