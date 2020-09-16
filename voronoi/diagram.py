7a607b4a1c940cc30952e5462704e2ce39efdf88
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-16 17:11:44 +0500

    Add ported 'Diagram.remove_edge' method

--- voronoi/diagram.py
+++ voronoi/diagram.py
@@ -20,3 +20,20 @@ class Diagram:
         self.vertices = [] if vertices is None else vertices
 
     __repr__ = generate_repr(__init__)
+
+    @staticmethod
+    def remove_edge(edge: Edge) -> None:
+        vertex = edge.start
+        cursor = edge.twin.rot_next
+        while cursor is not edge.twin:
+            cursor.start = vertex
+            cursor = cursor.rot_next
+        twin = edge.twin
+        edge_rot_prev, edge_rot_next = edge.rot_prev, edge.rot_next
+        twin_rot_prev, twin_rot_next = twin.rot_prev, twin.rot_next
+
+        # update prev/next pointers for the incident edges
+        edge_rot_next.twin.next = twin_rot_prev
+        twin_rot_prev.prev = edge_rot_next.twin
+        edge_rot_prev.prev = twin_rot_next.twin
+        twin_rot_next.twin.next = edge_rot_prev
