7802d96a074e80b4880fe1a4427f22850d00b53b
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-16 17:18:44 +0500

    Add ported 'Diagram.is_primary' method

--- voronoi/diagram.py
+++ voronoi/diagram.py
@@ -3,6 +3,7 @@ from typing import (List,
 
 from reprit.base import generate_repr
 
+from .events import SiteEvent
 from .faces import (Cell,
                     Edge,
                     Vertex)
@@ -20,6 +21,18 @@ class Diagram:
         self.vertices = [] if vertices is None else vertices
 
     __repr__ = generate_repr(__init__)
+
+    @staticmethod
+    def is_primary(first_event: SiteEvent, second_event: SiteEvent) -> bool:
+        first_event_is_segment, second_event_is_segment = (
+            first_event.is_segment, second_event.is_segment)
+        if first_event_is_segment and not second_event_is_segment:
+            return (first_event.start != second_event.start
+                    and first_event.end != second_event.start)
+        elif not first_event_is_segment and second_event_is_segment:
+            return (second_event.start != first_event.start
+                    and second_event.end != first_event.start)
+        return True
 
     @staticmethod
     def remove_edge(edge: Edge) -> None:
