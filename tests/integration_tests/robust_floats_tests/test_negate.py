e8113304d7b7af86593de956b2c244d452817b95
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-17 16:43:02 +0500

    Add test for bound & ported 'RobustFloat.__neg__' magic methods

---
+++ tests/integration_tests/robust_floats_tests/test_negate.py
@@ -0,0 +1,12 @@
+from hypothesis import given
+
+from tests.utils import (BoundPortedRobustFloatsPair,
+                         are_bound_ported_robust_floats_equal)
+from . import strategies
+
+
+@given(strategies.non_negative_robust_floats_pairs)
+def test_basic(pair: BoundPortedRobustFloatsPair) -> None:
+    bound, ported = pair
+
+    assert are_bound_ported_robust_floats_equal(-bound, -ported)
