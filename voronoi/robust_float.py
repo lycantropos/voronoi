b1d9ba2dafae508cc487ffb2e3e6af2730ee13f5
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-17 13:49:31 +0500

    Add ported 'RobustFloat' class

---
+++ voronoi/robust_float.py
@@ -0,0 +1,51 @@
+from math import sqrt
+
+from reprit.base import generate_repr
+
+ROUNDING_ERROR = 1
+
+
+class RobustFloat:
+    __slots__ = 'value', 'relative_error'
+
+    def __init__(self, value: float = 0., relative_error: float = 0.) -> None:
+        self.value = value
+        self.relative_error = relative_error
+
+    __repr__ = generate_repr(__init__)
+
+    def __add__(self, other: 'RobustFloat') -> 'RobustFloat':
+        value = self.value + other.value
+        relative_error = (max(self.relative_error, other.relative_error)
+                          + ROUNDING_ERROR
+                          if (not self.value or not other.value
+                              or (self.value > 0) is (other.value > 0))
+                          else abs((self.value * self.relative_error
+                                    - other.value * other.relative_error)
+                                   / value) + ROUNDING_ERROR)
+        return RobustFloat(value, relative_error)
+
+    def __mul__(self, other: 'RobustFloat') -> 'RobustFloat':
+        return RobustFloat(self.value * other.value,
+                           self.relative_error + other.relative_error
+                           + ROUNDING_ERROR)
+
+    def __sub__(self, other: 'RobustFloat') -> 'RobustFloat':
+        value = self.value - other.value
+        relative_error = (max(self.relative_error, other.relative_error)
+                          + ROUNDING_ERROR
+                          if (not self.value or not other.value
+                              or (self.value > 0) is not (other.value > 0))
+                          else abs((self.value * self.relative_error
+                                    + other.value * other.relative_error)
+                                   / value) + ROUNDING_ERROR)
+        return RobustFloat(value, relative_error)
+
+    def __truediv__(self, other: 'RobustFloat') -> 'RobustFloat':
+        return RobustFloat(self.value / other.value,
+                           self.relative_error + other.relative_error
+                           + ROUNDING_ERROR)
+
+    def sqrt(self) -> 'RobustFloat':
+        return RobustFloat(sqrt(self.value),
+                           self.relative_error * 0.5 + ROUNDING_ERROR)
