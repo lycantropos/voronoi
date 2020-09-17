6a6ee06bea620991b3b2456d6f1b56f52c84321f
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-17 12:45:42 +0500

    Add test for bound & ported 'BeachLineKey.__lt__' magic methods

---
+++ tests/integration_tests/beach_line_keys_tests/test_lower_than.py
@@ -0,0 +1,15 @@
+from hypothesis import given
+
+from tests.utils import (BoundPortedBeachLineKeysPair,
+                         equivalence)
+from . import strategies
+
+
+@given(strategies.beach_line_keys_pairs, strategies.beach_line_keys_pairs)
+def test_basic(first_pair: BoundPortedBeachLineKeysPair,
+               second_pair: BoundPortedBeachLineKeysPair) -> None:
+    first_bound, first_ported = first_pair
+    second_bound, second_ported = second_pair
+
+    assert equivalence(first_bound < second_bound,
+                       first_ported < second_ported)
