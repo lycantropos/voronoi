49ae018164f36e1dca61e40a5bda3a1341dfddf9
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-17 08:16:11 +0500

    Add test for bound & ported 'point_point_horizontal_goes_through_right_arc_first' functions

---
+++ tests/integration_tests/events_tests/test_point_point_horizontal_goes_through_right_arc_first.py
@@ -0,0 +1,24 @@
+from _voronoi import (point_point_horizontal_goes_through_right_arc_first
+                      as bound)
+from hypothesis import given
+
+from tests.utils import (BoundPortedPointsPair,
+                         BoundPortedSiteEventsPair,
+                         equivalence)
+from voronoi.events import (point_point_horizontal_goes_through_right_arc_first
+                            as ported)
+from . import strategies
+
+
+@given(strategies.site_events_pairs, strategies.site_events_pairs,
+       strategies.points_pairs)
+def test_basic(left_sites_pair: BoundPortedSiteEventsPair,
+               right_sites_pair: BoundPortedSiteEventsPair,
+               points_pair: BoundPortedPointsPair) -> None:
+    bound_left_site, ported_left_site = left_sites_pair
+    bound_right_site, ported_right_site = right_sites_pair
+    bound_point, ported_point = points_pair
+
+    assert equivalence(bound(bound_left_site, bound_right_site, bound_point),
+                       ported(ported_left_site, ported_right_site,
+                              ported_point))
