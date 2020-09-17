451f0d6cf64fa11330f26082e0df8a7b6cfc76ab
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-17 12:28:46 +0500

    Add test for bound & ported 'BeachLineKey.to_comparison_y' methods

---
+++ tests/integration_tests/beach_line_keys_tests/test_to_comparison_y.py
@@ -0,0 +1,12 @@
+from hypothesis import given
+
+from tests.utils import BoundPortedBeachLineKeysPair
+from . import strategies
+
+
+@given(strategies.beach_line_keys_pairs, strategies.booleans)
+def test_basic(pair: BoundPortedBeachLineKeysPair, is_new_node: bool) -> None:
+    bound, ported = pair
+
+    assert (bound.to_comparison_y(is_new_node)
+            == ported.to_comparison_y(is_new_node))
