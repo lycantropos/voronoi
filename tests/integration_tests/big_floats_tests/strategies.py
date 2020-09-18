bfba66bf56e8bba17fd17b9c0bf6bb9dd9d06e25
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-18 17:00:53 +0500

    Complete strategies

--- tests/integration_tests/big_floats_tests/strategies.py
+++ tests/integration_tests/big_floats_tests/strategies.py
@@ -8,3 +8,6 @@ doubles = doubles
 integers_32 = integers_32
 big_floats_pairs = strategies.builds(to_bound_with_ported_big_floats_pair,
                                      doubles, integers_32)
+non_zero_big_floats_pairs = strategies.builds(
+        to_bound_with_ported_big_floats_pair, doubles.filter(bool),
+        integers_32)
