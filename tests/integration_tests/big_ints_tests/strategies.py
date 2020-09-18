1c55f712bcd23b23820d27c25ab5712d19d88a9a
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-18 21:17:45 +0500

    Add test for bound & ported 'robust_product_with_sqrt' functions

--- tests/integration_tests/big_ints_tests/strategies.py
+++ tests/integration_tests/big_ints_tests/strategies.py
@@ -7,7 +7,10 @@ from voronoi.big_int import MAX_DIGITS_COUNT
 
 integers_32 = integers_32
 signs = strategies.sampled_from([-1, 0, 1])
+non_negative_signs = strategies.sampled_from([0, 1])
 digits = strategies.lists(unsigned_integers_32,
                           max_size=MAX_DIGITS_COUNT)
 big_ints_pairs = strategies.builds(to_bound_with_ported_big_ints_pair, signs,
                                    digits)
+non_negative_big_ints_pairs = strategies.builds(
+        to_bound_with_ported_big_ints_pair, non_negative_signs, digits)
---
+++ tests/integration_tests/big_ints_tests/test_robust_product_with_sqrt.py
@@ -0,0 +1,19 @@
+from _voronoi import robust_product_with_sqrt as bound
+from hypothesis import given
+
+from tests.utils import (BoundPortedBigIntsPair,
+                         are_bound_ported_big_floats_equal)
+from voronoi.big_int import robust_product_with_sqrt as ported
+from . import strategies
+
+
+@given(strategies.big_ints_pairs, strategies.non_negative_big_ints_pairs)
+def test_basic(first_pair: BoundPortedBigIntsPair,
+               second_pair: BoundPortedBigIntsPair) -> None:
+    first_bound, first_ported = first_pair
+    second_bound, second_ported = second_pair
+
+    bound_result = bound(first_bound, second_bound)
+    ported_result = ported(first_ported, second_ported)
+
+    assert are_bound_ported_big_floats_equal(bound_result, ported_result)
