0791b66d5594d035aff05324ec040a6bcad0f707
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-18 17:01:37 +0500

    Complete tests utilities

--- tests/utils.py
+++ tests/utils.py
@@ -166,7 +166,7 @@ def are_bound_ported_beach_line_keys_equal(bound: BoundBeachLineKey,
 
 def are_bound_ported_big_floats_equal(bound: BoundBigFloat,
                                       ported: PortedBigFloat) -> bool:
-    return (are_floats_equivalent(bound.mantissa, ported.mantissa)
+    return (bound.mantissa == ported.mantissa
             and bound.exponent == ported.exponent)
 
 
