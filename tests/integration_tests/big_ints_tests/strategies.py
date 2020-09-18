55802799dd3a4efa6d8fe31c47fb6d8101eb064a
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-18 21:16:23 +0500

    Complete strategies

--- tests/integration_tests/big_ints_tests/strategies.py
+++ tests/integration_tests/big_ints_tests/strategies.py
@@ -6,7 +6,7 @@ from tests.utils import to_bound_with_ported_big_ints_pair
 from voronoi.big_int import MAX_DIGITS_COUNT
 
 integers_32 = integers_32
-signs = strategies.sampled_from([-1, 0, -1])
+signs = strategies.sampled_from([-1, 0, 1])
 digits = strategies.lists(unsigned_integers_32,
                           max_size=MAX_DIGITS_COUNT)
 big_ints_pairs = strategies.builds(to_bound_with_ported_big_ints_pair, signs,
