cc629fe3b7ca07b8d819e2976e0ba81b9bde085a
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-18 18:43:56 +0500

    Add ported 'point_point_segment_circle_exists' function

--- voronoi/predicates.py
+++ voronoi/predicates.py
@@ -78,6 +78,28 @@ def point_point_point_circle_exists(first_site: SiteEvent,
                           third_site.start) is Orientation.RIGHT
 
 
+def point_point_segment_circle_exists(first_site: SiteEvent,
+                                      second_site: SiteEvent,
+                                      third_site: SiteEvent,
+                                      segment_index: int) -> bool:
+    if segment_index == 2:
+        return (third_site.start != first_site.start
+                or third_site.end != second_site.start)
+    else:
+        first_orientation = to_orientation(first_site.start, second_site.start,
+                                           third_site.start)
+        second_orientation = to_orientation(first_site.start,
+                                            second_site.start,
+                                            third_site.end)
+        if segment_index == 1 and first_site.start.x >= second_site.start.x:
+            return first_orientation is Orientation.RIGHT
+        elif segment_index == 3 and second_site.start.x >= first_site.start.x:
+            return second_orientation is Orientation.RIGHT
+        else:
+            return (first_orientation is Orientation.RIGHT
+                    or second_orientation is Orientation.RIGHT)
+
+
 def point_segment_horizontal_goes_through_right_arc_first(
         left_site: SiteEvent,
         right_site: SiteEvent,
