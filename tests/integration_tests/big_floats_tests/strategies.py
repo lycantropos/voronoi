384ba76daaf91ec4b7b72534eccf9d502f4606c9
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-18 20:45:28 +0500

    Add test for bound & ported 'BigFloat.sqrt' methods

--- tests/integration_tests/big_floats_tests/strategies.py
+++ tests/integration_tests/big_floats_tests/strategies.py
@@ -8,6 +8,8 @@ doubles = doubles
 integers_32 = integers_32
 big_floats_pairs = strategies.builds(to_bound_with_ported_big_floats_pair,
                                      doubles, integers_32)
+non_negative_big_floats_pairs = strategies.builds(
+        to_bound_with_ported_big_floats_pair, doubles.map(abs), integers_32)
 non_zero_big_floats_pairs = strategies.builds(
         to_bound_with_ported_big_floats_pair, doubles.filter(bool),
         integers_32)
