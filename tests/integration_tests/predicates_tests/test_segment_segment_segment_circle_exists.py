fac17f642abd6f611c80cf2aae6262db4458d04e
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-18 19:01:49 +0500

    Add test for bound & ported 'segment_segment_segment_circle_exists' functions

---
+++ tests/integration_tests/predicates_tests/test_segment_segment_segment_circle_exists.py
@@ -0,0 +1,22 @@
+from _voronoi import segment_segment_segment_circle_exists as bound
+from hypothesis import given
+
+from tests.utils import (BoundPortedSiteEventsPair,
+                         equivalence)
+from voronoi.predicates import segment_segment_segment_circle_exists as ported
+from . import strategies
+
+
+@given(strategies.site_events_pairs, strategies.site_events_pairs,
+       strategies.site_events_pairs)
+def test_basic(first_sites_pair: BoundPortedSiteEventsPair,
+               second_sites_pair: BoundPortedSiteEventsPair,
+               third_sites_pair: BoundPortedSiteEventsPair) -> None:
+    bound_first_site, ported_first_site = first_sites_pair
+    bound_second_site, ported_second_site = second_sites_pair
+    bound_third_site, ported_third_site = third_sites_pair
+
+    assert equivalence(bound(bound_first_site, bound_second_site,
+                             bound_third_site),
+                       ported(ported_first_site, ported_second_site,
+                              ported_third_site))
