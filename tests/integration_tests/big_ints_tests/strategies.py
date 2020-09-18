c43cd4750681c7026fe8bdaf7caf120d53ca7939
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-18 15:06:45 +0500

    Add test for bound & ported 'BigInt' classes initializations from integers

--- tests/integration_tests/big_ints_tests/strategies.py
+++ tests/integration_tests/big_ints_tests/strategies.py
@@ -1,9 +1,11 @@
 from hypothesis import strategies
 
-from tests.strategies import unsigned_integers_32
+from tests.strategies import (integers_32,
+                              unsigned_integers_32)
 from tests.utils import to_bound_with_ported_big_ints_pair
 from voronoi.big_int import MAX_DIGITS_COUNT
 
+integers_32 = integers_32
 signs = strategies.sampled_from([-1, 0, -1])
 digits = strategies.lists(unsigned_integers_32,
                           max_size=MAX_DIGITS_COUNT)
---
+++ tests/integration_tests/big_ints_tests/test_from_int.py
@@ -0,0 +1,15 @@
+from typing import List
+
+from hypothesis import given
+
+from tests.utils import (BoundBigInt,
+                         PortedBigInt,
+                         are_bound_ported_big_ints_equal)
+from . import strategies
+
+
+@given(strategies.integers_32)
+def test_basic(value: int) -> None:
+    bound, ported = BoundBigInt(value), PortedBigInt.from_int(value)
+
+    assert are_bound_ported_big_ints_equal(bound, ported)
