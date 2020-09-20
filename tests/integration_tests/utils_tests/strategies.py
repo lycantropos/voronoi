797496fdd8a1d8f2d34f3081991b037b28aac413
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-20 22:44:25 +0500

    Complete test for bound & ported 'robust_cross_product' functions

--- tests/integration_tests/utils_tests/strategies.py
+++ tests/integration_tests/utils_tests/strategies.py
@@ -2,10 +2,12 @@ from hypothesis import strategies
 
 from tests.strategies import (doubles,
                               integers_32,
+                              integers_64,
                               sizes)
 from tests.utils import to_bound_with_ported_points_pair
 
 coordinates = integers_32
+integers_64 = integers_64
 sizes = sizes
 floats = doubles
 points_pairs = strategies.builds(to_bound_with_ported_points_pair,
--- tests/integration_tests/utils_tests/test_robust_cross_product.py
+++ tests/integration_tests/utils_tests/test_robust_cross_product.py
@@ -5,11 +5,13 @@ from voronoi.utils import robust_cross_product as ported
 from . import strategies
 
 
-@given(strategies.coordinates, strategies.coordinates, strategies.coordinates,
-       strategies.coordinates)
+@given(strategies.integers_64, strategies.integers_64, strategies.integers_64,
+       strategies.integers_64)
 def test_basic(first_dx: int,
                first_dy: int,
                second_dx: int,
                second_dy: int) -> None:
-    assert (bound(first_dx, first_dy, second_dx, second_dy)
-            == ported(first_dx, first_dy, second_dx, second_dy))
+    bound_result = bound(first_dx, first_dy, second_dx, second_dy)
+    ported_result = ported(first_dx, first_dy, second_dx, second_dy)
+
+    assert bound_result == ported_result
