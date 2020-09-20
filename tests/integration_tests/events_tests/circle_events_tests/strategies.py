141b7f4ee18d53e9be1d09d177817973e4ecc1a2
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-21 01:26:41 +0500

    Add test for bound & ported 'CircleEvent.lies_outside_vertical_segment' methods

--- tests/integration_tests/events_tests/circle_events_tests/strategies.py
+++ tests/integration_tests/events_tests/circle_events_tests/strategies.py
@@ -1,10 +1,23 @@
 from hypothesis import strategies
 
-from tests.strategies import doubles
-from tests.utils import to_bound_with_ported_circle_events_pair
+from tests.strategies import (doubles,
+                              integers_32,
+                              sizes)
+from tests.utils import (bound_source_categories,
+                         ported_source_categories,
+                         to_bound_with_ported_circle_events_pair,
+                         to_bound_with_ported_points_pair,
+                         to_bound_with_ported_site_events_pair)
 
 booleans = strategies.booleans()
-doubles = doubles
+coordinates = integers_32
+points_pairs = strategies.builds(to_bound_with_ported_points_pair,
+                                 coordinates, coordinates)
+source_categories_pairs = strategies.sampled_from(
+        list(zip(bound_source_categories, ported_source_categories)))
 circle_events_pairs = strategies.builds(
         to_bound_with_ported_circle_events_pair, doubles, doubles, doubles,
         booleans)
+site_events_pairs = strategies.builds(to_bound_with_ported_site_events_pair,
+                                      points_pairs, points_pairs, sizes, sizes,
+                                      booleans, source_categories_pairs)
---
+++ tests/integration_tests/events_tests/circle_events_tests/test_lies_outside_vertical_segment.py
@@ -0,0 +1,16 @@
+from hypothesis import given
+
+from tests.utils import (BoundPortedCircleEventsPair,
+                         BoundPortedSiteEventsPair,
+                         equivalence)
+from . import strategies
+
+
+@given(strategies.circle_events_pairs, strategies.site_events_pairs)
+def test_basic(first_pair: BoundPortedCircleEventsPair,
+               sites_pair: BoundPortedSiteEventsPair) -> None:
+    bound, ported = first_pair
+    bound_site, ported_site = sites_pair
+
+    assert equivalence(bound.lies_outside_vertical_segment(bound_site),
+                       ported.lies_outside_vertical_segment(ported_site))
