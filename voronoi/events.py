5e9c75a3bc38611ae83273987daef3e496807097
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-21 01:24:35 +0500

    Complete members order for ported 'events' module

--- voronoi/events.py
+++ voronoi/events.py
@@ -28,73 +28,6 @@ from .utils import (compare_floats,
 ULPS = 64
 
 
-class CircleEvent:
-    __slots__ = 'center_x', 'center_y', 'lower_x', 'is_active'
-
-    def __init__(self,
-                 center_x: float,
-                 center_y: float,
-                 lower_x: float,
-                 is_active: bool = True) -> None:
-        self.center_x = center_x
-        self.center_y = center_y
-        self.lower_x = lower_x
-        self.is_active = is_active
-
-    __repr__ = generate_repr(__init__)
-
-    def __eq__(self, other: 'CircleEvent') -> bool:
-        return (self.center_x == other.center_x
-                and self.center_y == other.center_y
-                and self.lower_x == other.lower_x
-                and self.is_active is other.is_active
-                if isinstance(other, CircleEvent)
-                else NotImplemented)
-
-    def __lt__(self, other: 'Event') -> bool:
-        return ((self.lower_x, self.y) < (other.lower_x, other.y)
-                if isinstance(other, CircleEvent)
-                else (compare_floats(float(self.lower_x), float(other.start.x),
-                                     ULPS) is ComparisonResult.LESS
-                      if isinstance(other, SiteEvent)
-                      else NotImplemented))
-
-    @property
-    def lower_y(self) -> float:
-        return self.center_y
-
-    @property
-    def x(self) -> float:
-        return self.center_x
-
-    @x.setter
-    def x(self, value: float) -> None:
-        self.center_x = value
-
-    @property
-    def y(self) -> float:
-        return self.center_y
-
-    @y.setter
-    def y(self, value: float) -> None:
-        self.center_y = value
-
-    def lies_outside_vertical_segment(self, site: 'SiteEvent') -> bool:
-        if not site.is_segment or not site.is_vertical:
-            return False
-        start_y, end_y = ((site.end.y, site.start.y)
-                          if site.is_inverse
-                          else (site.start.y, site.end.y))
-        return (compare_floats(self.y, float(start_y),
-                               ULPS) is ComparisonResult.LESS
-                or compare_floats(self.y, float(end_y),
-                                  ULPS) is ComparisonResult.MORE)
-
-    def deactivate(self) -> 'CircleEvent':
-        self.is_active = False
-        return self
-
-
 class SiteEvent:
     __slots__ = ('start', 'end', 'sorted_index', 'initial_index', 'is_inverse',
                  'source_category')
@@ -171,6 +104,73 @@ class SiteEvent:
     def inverse(self) -> 'SiteEvent':
         self.start, self.end, self.is_inverse = (self.end, self.start,
                                                  not self.is_inverse)
+        return self
+
+
+class CircleEvent:
+    __slots__ = 'center_x', 'center_y', 'lower_x', 'is_active'
+
+    def __init__(self,
+                 center_x: float,
+                 center_y: float,
+                 lower_x: float,
+                 is_active: bool = True) -> None:
+        self.center_x = center_x
+        self.center_y = center_y
+        self.lower_x = lower_x
+        self.is_active = is_active
+
+    __repr__ = generate_repr(__init__)
+
+    def __eq__(self, other: 'CircleEvent') -> bool:
+        return (self.center_x == other.center_x
+                and self.center_y == other.center_y
+                and self.lower_x == other.lower_x
+                and self.is_active is other.is_active
+                if isinstance(other, CircleEvent)
+                else NotImplemented)
+
+    def __lt__(self, other: 'Event') -> bool:
+        return ((self.lower_x, self.y) < (other.lower_x, other.y)
+                if isinstance(other, CircleEvent)
+                else (compare_floats(float(self.lower_x), float(other.start.x),
+                                     ULPS) is ComparisonResult.LESS
+                      if isinstance(other, SiteEvent)
+                      else NotImplemented))
+
+    @property
+    def lower_y(self) -> float:
+        return self.center_y
+
+    @property
+    def x(self) -> float:
+        return self.center_x
+
+    @x.setter
+    def x(self, value: float) -> None:
+        self.center_x = value
+
+    @property
+    def y(self) -> float:
+        return self.center_y
+
+    @y.setter
+    def y(self, value: float) -> None:
+        self.center_y = value
+
+    def lies_outside_vertical_segment(self, site: SiteEvent) -> bool:
+        if not site.is_segment or not site.is_vertical:
+            return False
+        start_y, end_y = ((site.end.y, site.start.y)
+                          if site.is_inverse
+                          else (site.start.y, site.end.y))
+        return (compare_floats(self.y, float(start_y),
+                               ULPS) is ComparisonResult.LESS
+                or compare_floats(self.y, float(end_y),
+                                  ULPS) is ComparisonResult.MORE)
+
+    def deactivate(self) -> 'CircleEvent':
+        self.is_active = False
         return self
 
 
