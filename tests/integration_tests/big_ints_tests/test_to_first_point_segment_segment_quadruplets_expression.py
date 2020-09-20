23064a5ce0f160da74c49ad7fb10bb7c3b644b60
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-20 13:44:00 +0500

    Add test for bound & ported 'to_first_point_segment_segment_quadruplets_expression' functions

---
+++ tests/integration_tests/big_ints_tests/test_to_first_point_segment_segment_quadruplets_expression.py
@@ -0,0 +1,22 @@
+from _voronoi import (to_first_point_segment_segment_quadruplets_expression
+                      as bound)
+from hypothesis import given
+
+from tests.utils import (BoundPortedBigIntsQuadrupletsPair,
+                         are_bound_ported_big_floats_equal)
+from voronoi.big_int import (
+    to_first_point_segment_segment_quadruplets_expression as ported)
+from . import strategies
+
+
+@given(strategies.big_ints_pairs_quadruplets,
+       strategies.non_negative_big_ints_pairs_quadruplets)
+def test_basic(first_pair: BoundPortedBigIntsQuadrupletsPair,
+               second_pair: BoundPortedBigIntsQuadrupletsPair) -> None:
+    first_bound, first_ported = first_pair
+    second_bound, second_ported = second_pair
+
+    bound_result = bound(first_bound, second_bound)
+    ported_result = ported(first_ported, second_ported)
+
+    assert are_bound_ported_big_floats_equal(bound_result, ported_result)
