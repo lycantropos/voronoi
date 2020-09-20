efa197482290b5aad475e20f8e5e3aa0d36dd904
Azat Ibrakov <azatibrakov@gmail.com>
2020-09-20 23:32:26 +0500

    Complete test for bound & ported 'RobustFloat.sqrt' methods

--- tests/integration_tests/robust_floats_tests/strategies.py
+++ tests/integration_tests/robust_floats_tests/strategies.py
@@ -4,8 +4,5 @@ from tests.strategies import doubles
 from tests.utils import to_bound_with_ported_robust_floats_pair
 
 doubles = doubles
-non_negative_doubles = doubles.map(abs)
 robust_floats_pairs = strategies.builds(
         to_bound_with_ported_robust_floats_pair, doubles, doubles)
-non_negative_robust_floats_pairs = strategies.builds(
-        to_bound_with_ported_robust_floats_pair, non_negative_doubles, doubles)
