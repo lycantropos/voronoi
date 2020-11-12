a00ff1c2103738bd647c3b5fb4755f7978cd94d4
Azat Ibrakov <azatibrakov@gmail.com>
2020-11-12 16:43:17 +0300

    Complete test for bound & ported 'Builder.insert_point' methods

--- tests/integration_tests/builder_tests/test_insert_point.py
+++ tests/integration_tests/builder_tests/test_insert_point.py
@@ -1,14 +1,19 @@
 from hypothesis import given
 
 from tests.utils import (BoundPortedBuildersPair,
+                         BoundPortedPointsPair,
                          are_bound_ported_builders_equal)
 from . import strategies
 
 
-@given(strategies.builders_pairs, strategies.coordinates,
-       strategies.coordinates)
-def test_basic(pair: BoundPortedBuildersPair, x: int, y: int) -> None:
+@given(strategies.builders_pairs, strategies.points_pairs)
+def test_basic(pair: BoundPortedBuildersPair,
+               points_pair: BoundPortedPointsPair) -> None:
     bound, ported = pair
+    bound_point, ported_point = points_pair
 
-    assert bound.insert_point(x, y) == ported.insert_point(x, y)
+    bound_result = bound.insert_point(bound_point)
+    ported_result = ported.insert_point(ported_point)
+
+    assert bound_result == ported_result
     assert are_bound_ported_builders_equal(bound, ported)
