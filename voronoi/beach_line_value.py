0338569fabfa0060503719e5ff9e422e641a7d1d
Azat Ibrakov <azatibrakov@gmail.com>
2020-11-18 04:37:50 +0300

    Complete formatting

--- voronoi/beach_line_value.py
+++ voronoi/beach_line_value.py
@@ -7,7 +7,8 @@ from .faces import Edge
 
 
 class BeachLineValue:
-    def __init__(self, edge: Optional[Edge],
+    def __init__(self,
+                 edge: Optional[Edge],
                  circle_event: Optional[CircleEvent] = None) -> None:
         self.edge = edge
         self.circle_event = circle_event
