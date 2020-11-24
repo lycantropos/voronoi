ad147c178450eb8c58fb335e95d53a8cce57d2e4
Azat Ibrakov <azatibrakov@gmail.com>
2020-11-25 02:45:57 +0300

    Complete strategies

--- tests/integration_tests/faces_tests/cells_tests/strategies.py
+++ tests/integration_tests/faces_tests/cells_tests/strategies.py
@@ -1,15 +1,10 @@
 from hypothesis import strategies
 
-from tests.strategies import (doubles,
-                              sizes)
+from tests.strategies import sizes
 from tests.utils import (bound_source_categories,
                          ported_source_categories,
-                         to_bound_with_ported_cells_pair,
-                         to_pairs)
+                         to_bound_with_ported_cells_pair)
 
-booleans = strategies.booleans()
-coordinates = doubles
-nones_pairs = to_pairs(strategies.none())
 source_categories_pairs = strategies.sampled_from(
         list(zip(bound_source_categories, ported_source_categories)))
 cells_pairs = strategies.builds(to_bound_with_ported_cells_pair, sizes,
--- tests/integration_tests/faces_tests/vertices_tests/strategies.py
+++ tests/integration_tests/faces_tests/vertices_tests/strategies.py
@@ -1,15 +1,8 @@
 from hypothesis import strategies
 
 from tests.strategies import doubles
-from tests.utils import (bound_source_categories,
-                         ported_source_categories,
-                         to_bound_with_ported_vertices_pair,
-                         to_pairs)
+from tests.utils import to_bound_with_ported_vertices_pair
 
-booleans = strategies.booleans()
 coordinates = doubles
-nones_pairs = to_pairs(strategies.none())
-source_categories_pairs = strategies.sampled_from(
-        list(zip(bound_source_categories, ported_source_categories)))
 vertices_pairs = strategies.builds(to_bound_with_ported_vertices_pair,
                                    coordinates, coordinates)
