558229dbe99fb848499b5e12d473997fb27010bd
Azat Ibrakov <azatibrakov@gmail.com>
2020-11-25 00:59:42 +0300

    Complete bound 'Vertex.incident_edge' property

--- src/main.cpp
+++ src/main.cpp
@@ -844,9 +844,10 @@ PYBIND11_MODULE(MODULE_NAME, m) {
            py::arg("x"), py::arg("y"), py::arg("incident_edge") = nullptr)
       .def("__repr__", to_repr<Vertex>)
       .def(py::self == py::self)
-      .def_property_readonly(
+      .def_property(
           "incident_edge",
-          [](const Vertex& self) { return self.incident_edge(); })
+          [](const Vertex& self) { return self.incident_edge(); },
+          [](Vertex& self, Edge* value) { self.incident_edge(value); })
       .def_property_readonly("is_degenerate", &Vertex::is_degenerate)
       .def_property_readonly("x", &Vertex::x)
       .def_property_readonly("y", &Vertex::y);
