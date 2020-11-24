a2908ad561e02abb1dd315c870f3ef11e12cbbc8
Azat Ibrakov <azatibrakov@gmail.com>
2020-11-25 00:54:04 +0300

    Complete bound 'Edge' class initializer

--- src/main.cpp
+++ src/main.cpp
@@ -663,18 +663,15 @@ PYBIND11_MODULE(MODULE_NAME, m) {
       .def_property_readonly("vertices", &Diagram::vertices);
 
   py::class_<Edge, std::unique_ptr<Edge, py::nodelete>>(m, EDGE_NAME)
-      .def(py::init([](Vertex* start, Edge* twin, Edge* prev, Edge* next,
-                       Cell* cell, bool is_linear, bool is_primary) {
-             Edge result{is_linear, is_primary};
-             result.vertex0(start);
-             result.twin(twin);
-             result.prev(prev);
-             result.next(next);
-             result.cell(cell);
-             return result;
-           }),
-           py::arg("start"), py::arg("twin"), py::arg("prev"), py::arg("next"),
-           py::arg("cell"), py::arg("is_linear"), py::arg("is_primary"))
+      .def(py::init(
+               [](Vertex* start, Cell* cell, bool is_linear, bool is_primary) {
+                 Edge result{is_linear, is_primary};
+                 result.vertex0(start);
+                 result.cell(cell);
+                 return result;
+               }),
+           py::arg("start"), py::arg("cell"), py::arg("is_linear"),
+           py::arg("is_primary"))
       .def("__repr__", to_repr<Edge>)
       .def_property_readonly("cell",
                              [](const Edge& self) { return self.cell(); })
