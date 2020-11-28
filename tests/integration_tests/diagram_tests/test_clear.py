93ba294447578cd71cec04c799243498865157b4
Azat Ibrakov <azatibrakov@gmail.com>
2020-11-28 10:38:41 +0300

    Complete test for bound & ported 'Diagram.clear' methods

--- tests/integration_tests/diagram_tests/test_clear.py
+++ tests/integration_tests/diagram_tests/test_clear.py
@@ -2,6 +2,7 @@ from hypothesis import given
 
 from tests.integration_tests.hints import BoundPortedDiagramsPair
 from tests.integration_tests.utils import are_bound_ported_diagrams_equal
+from tests.utils import equivalence
 from . import strategies
 
 
@@ -9,4 +10,8 @@ from . import strategies
 def test_basic(pair: BoundPortedDiagramsPair) -> None:
     bound, ported = pair
 
+    bound_result = bound.clear()
+    ported_result = ported.clear()
+
+    assert equivalence(bound_result, ported_result)
     assert are_bound_ported_diagrams_equal(bound, ported)
