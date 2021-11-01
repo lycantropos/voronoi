5d8bd554d0d6bfc2a0ccf4d4fa43cdf4cc69ef05
Azat Ibrakov <azatibrakov@gmail.com>
2021-11-01 20:36:08 +0300

    Complete 'setup.py' script

--- setup.py
+++ setup.py
@@ -46,47 +46,50 @@ if platform.python_implementation() == 'CPython':
     from distutils.ccompiler import CCompiler
     from distutils.errors import CompileError
     from glob import glob
-    from setuptools import Extension
+    from typing import (Any,
+                        Iterable)
+
+    from setuptools import (Command,
+                            Extension)
     from setuptools.command.build_ext import build_ext
+    from setuptools.command.develop import develop
 
 
-    def has_flag(compiler: CCompiler, name: str) -> bool:
-        """
-        Detects whether a flag name is supported on the specified compiler.
-        """
+    def is_flag_supported(flag: str, compiler: CCompiler) -> bool:
         with tempfile.NamedTemporaryFile('w',
                                          suffix='.cpp') as file:
-            file.write('int main (int argc, char **argv) { return 0; }')
+            file.write('int main(void) { return 0; }')
             try:
                 compiler.compile([file.name],
-                                 extra_postargs=[name])
+                                 extra_postargs=[flag])
             except CompileError:
                 return False
         return True
 
 
-    def cpp_flag(compiler: CCompiler) -> str:
-        """
-        Returns the -std=c++[11|...] compiler flag.
-        The newer version is preferred when available.
-        """
-        flags = ['-std=c++{}'.format(str(year)[2:])
-                 for year in range(2011, date.today().year + 1, 3)]
-        for flag in reversed(flags):
-            if has_flag(compiler, flag):
-                return flag
-        raise RuntimeError('Unsupported compiler: '
-                           'at least C++11 support is needed.')
+    def to_first_supported_flag(flags: Iterable[str],
+                                compiler: CCompiler) -> str:
+        flags = list(flags)
+        try:
+            return next(flag
+                        for flag in flags
+                        if is_flag_supported(flag, compiler))
+        except StopIteration:
+            quote = '"{}"'.format
+            raise CompileError('None of {flags} flags are supported '
+                               'by {compiler} compiler.'
+                               .format(flags=', '.join(map(quote, flags)),
+                                       compiler=quote(compiler.compiler_type)))
+
+
+    def year_to_standard(year: int) -> str:
+        return 'c++{}'.format(str(year)[2:])
 
 
     class BuildExt(build_ext):
         """A custom build extension for adding compiler-specific options."""
-        compile_args = defaultdict(list,
-                                   {'msvc': ['/EHsc'],
-                                    'unix': []})
-        link_args = defaultdict(list,
-                                {'msvc': [],
-                                 'unix': []})
+        compile_args = defaultdict(list, {'msvc': ['/EHsc'], 'unix': []})
+        link_args = defaultdict(list, {'msvc': [], 'unix': []})
 
         if sys.platform == 'darwin':
             darwin_args = ['-stdlib=libc++', '-mmacosx-version-min=10.7']
@@ -97,16 +100,49 @@ if platform.python_implementation() == 'CPython':
             compiler_type = self.compiler.compiler_type
             compile_args = self.compile_args[compiler_type]
             link_args = self.link_args[compiler_type]
+            cpp_standards = [
+                year_to_standard(year)
+                for year in reversed(range(2017, date.today().year + 1, 3))]
             if compiler_type == 'unix':
-                compile_args.append(cpp_flag(self.compiler))
-                if has_flag(self.compiler, '-fvisibility=hidden'):
+                compile_args.append(
+                        to_first_supported_flag(
+                                ['-std={}'.format(standard)
+                                 for standard in cpp_standards],
+                                self.compiler))
+                if is_flag_supported('-fvisibility=hidden', self.compiler):
                     compile_args.append('-fvisibility=hidden')
+            elif compiler_type == 'msvc':
+                try:
+                    standard_flag = to_first_supported_flag(
+                            ['/std:{}'.format(standard)
+                             for standard in cpp_standards],
+                            self.compiler)
+                except CompileError:
+                    compile_args.append('/std:c++latest')
+                else:
+                    compile_args.append(standard_flag)
             define_macros = [('VERSION_INFO', self.distribution.get_version())]
             for extension in self.extensions:
-                extension.extra_compile_args = compile_args
-                extension.extra_link_args = link_args
-                extension.define_macros = define_macros
+                extension.extra_compile_args += compile_args
+                extension.extra_link_args += link_args
+                extension.define_macros += define_macros
             super().build_extensions()
+
+
+    class Develop(develop):
+        def reinitialize_command(self,
+                                 name: str,
+                                 reinit_subcommands: int = 0,
+                                 **kwargs: Any) -> Command:
+            if name == build_ext.__name__:
+                kwargs.setdefault('debug', 1)
+            result = super().reinitialize_command(name, reinit_subcommands,
+                                                  **kwargs)
+            if name == build_ext.__name__:
+                result.ensure_finalized()
+                for extension in result.extensions:
+                    extension.undef_macros.append('NDEBUG')
+            return result
 
 
     class LazyPybindInclude:
@@ -115,12 +151,14 @@ if platform.python_implementation() == 'CPython':
             return pybind11.get_include()
 
 
+    project_root = Path(__file__).parent
     parameters.update(
-            cmdclass={'build_ext': BuildExt},
+            cmdclass={build_ext.__name__: BuildExt,
+                      develop.__name__: Develop},
             ext_modules=[Extension('_' + voronoi.__name__,
                                    glob('src/*.cpp'),
                                    include_dirs=[LazyPybindInclude(),
-                                                 Path.cwd() / 'include'],
+                                                 project_root / 'include'],
                                    language='c++')],
             zip_safe=False)
 setup(**parameters)
