560640bca300a8f632f6fb9d848c98e1b72b71bb
Azat Ibrakov <azatibrakov@gmail.com>
2021-11-02 18:42:15 +0300

    Complete test of ported 'Builder.construct' method

--- tests/port_tests/diagram_tests/test_construct.py
+++ tests/port_tests/diagram_tests/test_construct.py
@@ -9,14 +9,20 @@ from . import strategies
 @given(strategies.empty_diagrams, strategies.multipoints)
 def test_multipoints(diagram: PortedDiagram,
                      multipoint: PortedPointsList) -> None:
-    result = diagram.construct(multipoint, [])
-
-    assert result is None
+    try:
+        result = diagram.construct(multipoint, [])
+    except ValueError:
+        pass
+    else:
+        assert result is None
 
 
 @given(strategies.empty_diagrams, strategies.multisegments)
 def test_multisegments(diagram: PortedDiagram,
                        multisegment: PortedSegmentsList) -> None:
-    result = diagram.construct([], multisegment)
-
-    assert result is None
+    try:
+        result = diagram.construct([], multisegment)
+    except ValueError:
+        pass
+    else:
+        assert result is None
