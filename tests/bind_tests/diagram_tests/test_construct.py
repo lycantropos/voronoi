5d534c440e1c5e77d3fbef70d1bd5962aed4d05e
Azat Ibrakov <azatibrakov@gmail.com>
2021-11-02 18:42:09 +0300

    Complete test of bound 'Builder.construct' method

--- tests/bind_tests/diagram_tests/test_construct.py
+++ tests/bind_tests/diagram_tests/test_construct.py
@@ -9,14 +9,20 @@ from . import strategies
 @given(strategies.empty_diagrams, strategies.multipoints)
 def test_multipoints(diagram: BoundDiagram,
                      multipoint: BoundPointsList) -> None:
-    result = diagram.construct(multipoint, [])
-
-    assert result is None
+    try:
+        result = diagram.construct(multipoint, [])
+    except ValueError:
+        pass
+    else:
+        assert result is None
 
 
 @given(strategies.empty_diagrams, strategies.multisegments)
 def test_multisegments(diagram: BoundDiagram,
                        multisegment: BoundSegmentsList) -> None:
-    result = diagram.construct([], multisegment)
-
-    assert result is None
+    try:
+        result = diagram.construct([], multisegment)
+    except ValueError:
+        pass
+    else:
+        assert result is None
