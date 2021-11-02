2c37f71a44e1bf4920f03682116eec232337a88a
Azat Ibrakov <azatibrakov@gmail.com>
2021-11-02 18:39:20 +0300

    Complete test of bound & ported 'Builder.construct' methods

--- tests/integration_tests/builder_tests/test_construct.py
+++ tests/integration_tests/builder_tests/test_construct.py
@@ -1,3 +1,4 @@
+import pytest
 from hypothesis import given
 
 from tests.integration_tests.hints import (BoundPortedBuildersPair,
@@ -14,9 +15,14 @@ def test_basic(pair: BoundPortedBuildersPair,
     bound, ported = pair
     bound_diagram, ported_diagram = diagrams_pair
 
-    bound_result = bound.construct(bound_diagram)
-    ported_result = ported.construct(ported_diagram)
+    try:
+        bound_result = bound.construct(bound_diagram)
+    except ValueError:
+        with pytest.raises(ValueError):
+            ported.construct(ported_diagram)
+    else:
+        ported_result = ported.construct(ported_diagram)
 
-    assert equivalence(bound_result, ported_result)
-    assert are_bound_ported_builders_equal(bound, ported)
-    assert are_bound_ported_diagrams_equal(bound_diagram, ported_diagram)
+        assert equivalence(bound_result, ported_result)
+        assert are_bound_ported_builders_equal(bound, ported)
+        assert are_bound_ported_diagrams_equal(bound_diagram, ported_diagram)
