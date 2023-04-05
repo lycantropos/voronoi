88be8b25d7a1a35a0038419fbeee74ddbde02991
Azat Ibrakov <azatibrakov@gmail.com>
2023-04-05 04:09:25 +0200

    Fix tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,26 @@
+import os
+import sys
 from datetime import timedelta
 
+import pytest
 from hypothesis import (HealthCheck,
                         settings)
 
+is_pypy = sys.implementation.name == 'pypy'
+on_ci = bool(os.getenv('CI', False))
+max_examples = (-(-settings.default.max_examples // 5)
+                if is_pypy and on_ci
+                else settings.default.max_examples)
 settings.register_profile('default',
-                          deadline=timedelta(minutes=10),
-                          suppress_health_check=[HealthCheck.filter_too_much,
-                                                 HealthCheck.too_slow])
+                          deadline=(timedelta(hours=1) / max_examples
+                                    if on_ci
+                                    else None),
+                          max_examples=max_examples,
+                          suppress_health_check=[HealthCheck.too_slow])
+
+
+@pytest.hookimpl(trylast=True)
+def pytest_sessionfinish(session: pytest.Session,
+                         exitstatus: pytest.ExitCode) -> None:
+    if exitstatus == pytest.ExitCode.NO_TESTS_COLLECTED:
+        session.exitstatus = pytest.ExitCode.OK
